c8e899a6bbd6e6b879edc1a10241c106f5c0b871
Tomás Cañibano <tomas.cani@gmail.com>
2018-11-06 11:34:52 +0100

    Use querySelector and remove unvisited countries list from html

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const unvisitedCountries = countries;
 const visitedCountries = [];
 
 function createCountryList(countries) {
-  const countriesList = document.createElement('ul');
+  const countriesList = document.createElement('section');
+  countriesList.classList.add('countries-list');
   countries.forEach((country) => {
-    const countryElement = document.createElement('p');
+    const countryElement = document.createElement('div');
     const countryName = document.createTextNode(country.name);
     countryElement.appendChild(countryName);
     countriesList.appendChild(countryElement);
@@ -16,16 +17,13 @@ function filterCountries(countries, keyword) {
   return countries.filter(country => country.name.toLowerCase().includes(keyword.toLowerCase()));
 }
 
-function removeChildren(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
-
 function renderUnvisitedCountries(event) {
   const countriesList = createCountryList(filterCountries(unvisitedCountries, event.target.value));
-  const unvisitedCountriesElement = document.getElementsByClassName('unvisited-countries-list')[0];
-  removeChildren(unvisitedCountriesElement);
+  const unvisitedCountriesElement = document.querySelector('.unvisited-countries');
+  const unvisitedCountriesListElement = unvisitedCountriesElement.querySelector('.countries-list');
+  if (unvisitedCountriesListElement) {
+    unvisitedCountriesListElement.parentNode.removeChild(unvisitedCountriesListElement);
+  }
   unvisitedCountriesElement.appendChild(countriesList);
 }
 
